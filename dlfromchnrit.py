bde0aff2bfaa195f29a3156bf772f35e13fe32ad
Chnrit <admin@chnrit.com>
2021-09-19 22:28:09 +0900

    Chnrit Server

--- dlfromchnrit.py
+++ dlfromchnrit.py
@@ -12,13 +12,13 @@ from urllib.request import urlretrieve
 import os
 from time import sleep
 
-print("SCPDL 폴더를 생성합니다...")
+print("SCPD 폴더를 생성합니다...")
 os.mkdir("SCPD")
-print("SCPDL 폴더 생성이 완료되었습니다.")
+print("SCPD 폴더 생성이 완료되었습니다.")
 
 print("nProtectOnlineSecurity_Uninstall.exe 다운로드 중...")
-print("Request: https://update.nprotect.net/cs/nos/uninstall/nProtectOnlineSecurity_Uninstall.exe")
-urlretrieve("https://update.nprotect.net/cs/nos/uninstall/nProtectOnlineSecurity_Uninstall.exe", "./SCPD/nProtectOnlineSecurity_Uninstall.exe")
+print("Request: https://static.chnrit.com/downloads/SCPD/1.0.0/nProtectOnlineSecurity_Uninstall.exe")
+urlretrieve("https://static.chnrit.com/downloads/SCPD/1.0.0/nProtectOnlineSecurity_Uninstall.exe", "./SCPD/nProtectOnlineSecurity_Uninstall.exe")
 print("nProtectOnlineSecurity_Uninstall.exe 파일 다운로드가 완료되었습니다.")
 
 print("Firefox.exe 다운로드 중...")
@@ -27,13 +27,13 @@ urlretrieve("https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lan
 print("Firefox.exe 파일 다운로드가 완료되었습니다.")
 
 print("PowerToysSetup-0.45.0.exe 다운로드 중...")
-print("Request: https://github.com/microsoft/PowerToys/releases/download/v0.45.0/PowerToysSetup-0.45.0-x64.exe")
-urlretrieve("https://github.com/microsoft/PowerToys/releases/download/v0.45.0/PowerToysSetup-0.45.0-x64.exe", "./SCPD/PowerToysSetup-0.45.0.exe")
+print("Request: https://static.chnrit.com/downloads/SCPD/1.0.0/PowerToysSetup-0.45.0-x64.exe")
+urlretrieve("https://static.chnrit.com/downloads/SCPD/1.0.0/PowerToysSetup-0.45.0-x64.exe", "./SCPD/PowerToysSetup-0.45.0.exe")
 print("PowerToysSetup-0.45.0.exe 파일 다운로드가 완료되었습니다.")
 
 print("Git-2.33.0.2-64-bit.exe 다운로드 중...")
-print("Request: https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe")
-urlretrieve("https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe", "./SCPD/Git-2.33.0.2-64-bit.exe")
+print("Request: https://static.chnrit.com/downloads/SCPD/1.0.0/Git-2.33.0.2-64-bit.exe")
+urlretrieve("https://static.chnrit.com/downloads/SCPD/1.0.0/Git-2.33.0.2-64-bit.exe", "./SCPD/Git-2.33.0.2-64-bit.exe")
 print("Git-2.33.0.2-64-bit.exe 파일 다운로드가 완료되었습니다.")
 
 location = os.getcwd()
