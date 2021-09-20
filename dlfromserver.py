e9c8af626334606c294935cea1bcbf7e252ea14a
Chnrit <admin@chnrit.com>
2021-09-20 21:49:09 +0900

    파일 수정

--- dlfromserver.py
+++ dlfromserver.py
@@ -5,40 +5,57 @@
 # 
 # Author: Chnrit (Chnrit@github)
 # License: MIT
+# Version: 1.0.1.1
 # 
 ##############
 
 from urllib.request import urlretrieve
 import os
+import os.path
+import shutil
 from time import sleep
 
-print("SCPDL 폴더를 생성합니다...")
-os.mkdir("SCPD")
-print("SCPDL 폴더 생성이 완료되었습니다.")
+# download에 대한 함수를 정의
+def download(filename, fileurl):
+    print(filename + " 다운로드 중...")
+    print("요청: " + fileurl)
+    urlretrieve(fileurl, "./SCPD/" + filename)
+    print(filename + "파일 다운로드가 완료되었습니다.\n")
 
-print("nProtectOnlineSecurity_Uninstall.exe 다운로드 중...")
-print("Request: https://update.nprotect.net/cs/nos/uninstall/nProtectOnlineSecurity_Uninstall.exe")
-urlretrieve("https://update.nprotect.net/cs/nos/uninstall/nProtectOnlineSecurity_Uninstall.exe", "./SCPD/nProtectOnlineSecurity_Uninstall.exe")
-print("nProtectOnlineSecurity_Uninstall.exe 파일 다운로드가 완료되었습니다.")
+# 폴더 체크 단계
+dir = "SCPD"
+print(dir +" 폴더 생성을 시도합니다...")
+if os.path.isdir(dir):
+    print(dir + " 폴더가 이미 존재합니다.")
 
-print("Firefox.exe 다운로드 중...")
-print("Request: https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=ko")
-urlretrieve("https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=ko", "./SCPD/Firefox.exe")
-print("Firefox.exe 파일 다운로드가 완료되었습니다.")
+    SCPDFileList = os.listdir(dir)
+    modifiedFileList = ', '.join(SCPDFileList)
 
-print("PowerToysSetup-0.45.0.exe 다운로드 중...")
-print("Request: https://github.com/microsoft/PowerToys/releases/download/v0.45.0/PowerToysSetup-0.45.0-x64.exe")
-urlretrieve("https://github.com/microsoft/PowerToys/releases/download/v0.45.0/PowerToysSetup-0.45.0-x64.exe", "./SCPD/PowerToysSetup-0.45.0.exe")
-print("PowerToysSetup-0.45.0.exe 파일 다운로드가 완료되었습니다.")
+    print("폴더에 존재하는 파일: {}".format(modifiedFileList))
+    print("**중요** 폴더를 삭제한 후 프로그램을 다시 실행해 주세요.")
 
-print("Git-2.33.0.2-64-bit.exe 다운로드 중...")
-print("Request: https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe")
-urlretrieve("https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe", "./SCPD/Git-2.33.0.2-64-bit.exe")
-print("Git-2.33.0.2-64-bit.exe 파일 다운로드가 완료되었습니다.")
+    deldir = input("폴더를 삭제할까요? [Y/n] ")
 
-location = os.getcwd()
+    if deldir == "Y" or deldir == "y":
+        shutil.rmtree(dir)
+        print(dir + " 폴더를 삭제했습니다.")
+    else:
+        print(dir + " 폴더 삭제를 취소했습니다.")
+else:
+    os.mkdir("SCPD")
+    print("SCPD 폴더 생성이 완료되었습니다.\n")
 
-# open explore.exe
-print("자동으로 창이 닫히고 파일 탐색기가 열립니다...")
-sleep(0.5)
-os.startfile(location + "/SCPD/")
+    download("nProtectOnlineSecurity_Uninstall.exe", "https://update.nprotect.net/cs/nos/uninstall/nProtectOnlineSecurity_Uninstall.exe")
+
+    download("Firefox.exe", "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=ko")
+
+    download("PowerToysSetup-0.45.0.exe", "https://github.com/microsoft/PowerToys/releases/download/v0.45.0/PowerToysSetup-0.45.0-x64.exe")
+
+    download("Git-2.33.0.2-64-bit.exe", "https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe")
+
+    location = os.getcwd()
+
+    # open explore.exe
+    print("자동으로 창이 닫히고 파일 탐색기가 열립니다...")
+    sleep(0.5)
+    os.startfile(location + "/SCPD/")
